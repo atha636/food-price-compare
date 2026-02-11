eb58a4a619e02eba46221cacc38567a292c573f1
Atharv Patidar <atharvpatidar2005@gmail.com>
2026-02-11 16:16:56 +0530

    Fix Render port issue

--- server/index.js
+++ server/index.js
@@ -33,7 +33,8 @@ const swiggyPrice = getSwiggyPrice(item, city);
 });
 
 // start server
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
