946a60b8e06e2af8c027b2b9d4a46e14ae86095b
Atharv Patidar <atharvpatidar2005@gmail.com>
2026-02-24 23:23:36 +0530

    Added MongoDB connection

--- server/index.js
+++ server/index.js
@@ -1,3 +1,5 @@
+require("dotenv").config();
+const mongoose = require("mongoose");
 const express = require("express");
 const cors = require("cors");
 
@@ -99,8 +101,10 @@ const swiggyList = swiggyRestaurants.map(name => ({
 });
 
 
-const PORT = process.env.PORT;
-
+const PORT = process.env.PORT || 5000;
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("✅ MongoDB Connected"))
+  .catch(err => console.log("❌ MongoDB Error:", err));
 
 app.listen(PORT, "0.0.0.0", () => {
   console.log("Server running on port " + PORT);
